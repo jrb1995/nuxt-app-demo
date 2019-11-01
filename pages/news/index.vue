c93f1f5ed6c5b5fbef02686d195bca2d57e18553
mushan <mushan@cyipp.com>
2019-11-01 18:03:09 +0800

    init

---
+++ pages/news/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    新闻列表
+    <ul>
+      <li v-for="item of list">
+        <a :href="`/p?id=${item.id}`">{{ item.title }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      ip: '',
+      list: []
+    }
+  },
+  async asyncData (ctx) {
+    const result = await ctx.$axios.$get('https://cnodejs.org/api/v1/topics')
+    return { list: result.data }
+  },
+  mounted () {
+    
+  },
+  methods: {
+
+  }
+}
+</script>
---
+++ pages/p.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-html="content"></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      content: '2211'
+    }
+  },
+  async asyncData (ctx) {
+    let id = ctx.query.id
+    const result = await ctx.$axios.$get('https://cnodejs.org/api/v1/topic/' + id)
+    const content = result.data.content
+    return { content }
+  },
+}
+</script>
