b25a57968b49161716297c954a48c25cfc5397aa
jrb1995 <736275880@qq.com>
2019-11-06 15:43:35 +0800

    feat: 设置 html 头部 title

--- pages/news/_id.vue
+++ pages/news/_id.vue
@@ -6,14 +6,21 @@
 export default {
   data () {
     return {
+      title: '',
       content: '2211'
+    }
+  },
+  head () {
+    return {
+      title: this.title
     }
   },
   async asyncData (ctx) {
     let id = ctx.params.id
     const result = await ctx.$axios.$get('https://cnodejs.org/api/v1/topic/' + id)
     const content = result.data.content
-    return { content }
+    const title = result.data.title
+    return { content, title }
   },
 }
 </script>
